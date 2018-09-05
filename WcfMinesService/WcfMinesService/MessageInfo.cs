7ccddd3c50d61559b8f51d6c64717d0bb0523d48
nach66 <41806538+nach66@users.noreply.github.com>
2018-09-05 14:27:26 +0300

    Add files via upload

---
+++ WcfMinesService/WcfMinesService/MessageInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfMinesService
+{
+    [DataContract]
+    public class MessageInfo
+    {
+        [DataMember]
+        public string Message { get; set; }
+        [DataMember]
+        public string FromCLient { get; set; }
+        [DataMember]
+        public List<string> ToClients { get; set; }
+    }
+}
