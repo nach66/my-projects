60be2d8ab7b673765946176a70e025faf4f9763a
nach66 <41806538+nach66@users.noreply.github.com>
2018-09-05 14:08:58 +0300

    Add files via upload

---
+++ MinesHost/MinesHost/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace MinesHost
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ServiceHost host;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            host.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                host = new ServiceHost(typeof(WcfMinesService.GameService));
+                host.Description.Behaviors.Add(
+                    new ServiceMetadataBehavior { HttpGetEnabled = true });
+                host.Open();
+                label1.Content = "Service is running";
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+                this.Close();
+            }
+        }
+    }
+}
+
+
+
+
+
+
